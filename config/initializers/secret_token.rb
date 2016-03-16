76d99c1598f67ce6d98cc98f24472d26f83e911d
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-03-16 01:27:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letmecookforyou::Application.config.secret_key_base = 'a92be3377e09d39ddd7b85030cf42edf9cd3a659c5f98a65361d247de036727edecbffe24c20a1b47240967beab78375581f390afeff14827bb6a9f1be877b54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Letmecookforyou::Application.config.session_store :cookie_store, key: '_letmecookforyou_session'
