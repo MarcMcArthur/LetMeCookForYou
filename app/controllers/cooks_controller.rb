d17f95330ca33eeba54d6d75b3be537b75d96008
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-03-24 14:02:01 +0000

    Build a form to save new cooks

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -2,4 +2,9 @@ class CooksController < ApplicationController
   def index
     @cooks =Cook.all
   end
+
+  def new
+    @cooks = Cook.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Letmecookforyou::Application.routes.draw do
   root 'cooks#index'
+
+  resources :cooks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
